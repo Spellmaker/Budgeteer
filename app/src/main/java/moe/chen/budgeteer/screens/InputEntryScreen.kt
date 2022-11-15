1d542509f0e1d168d83a92769b285247ec3647d2
MarkusBrenner <markus.brenner@iv3.ai>
2022-11-15 13:44:15 +0100

    make input entry screen more usable

--- app/src/main/java/moe/chen/budgeteer/screens/InputEntryScreen.kt
+++ app/src/main/java/moe/chen/budgeteer/screens/InputEntryScreen.kt
@@ -1,17 +1,18 @@
 package moe.chen.budgeteer.screens
 
+import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.material.Surface
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,7 +23,6 @@ import moe.chen.budgeteer.formatCompact
 import moe.chen.budgeteer.room.Category
 import moe.chen.budgeteer.room.User
 import moe.chen.budgeteer.viewmodel.InputEntryViewModel
-import moe.chen.budgeteer.viewmodel.UserSettingViewModel
 import moe.chen.budgeteer.widgets.MainViewWidget
 
 @Composable
@@ -33,39 +33,51 @@ fun InputEntryScreen(
     logout: () -> Unit,
     accessSettings: () -> Unit,
 ) {
-    val amount = remember { mutableStateOf(0.0) }
+    var amountDouble by remember { mutableStateOf(0.0) }
     val model = hiltViewModel<InputEntryViewModel>()
     model.listenForCategory(categoryId)
     val category = model.category.collectAsState()
-
-    val settingsModel = hiltViewModel<UserSettingViewModel>()
-    settingsModel.listenToUser(user)
-
-    val converterDefault = settingsModel.converterDefault.collectAsState()
+    var amountString by remember {
+        mutableStateOf<String>(
+            formatCompact(0.0)
+        )
+    }
+    var isValid by remember { mutableStateOf(true) }
 
     MainViewWidget(logout = logout, settings = accessSettings) {
         if (category.value != null) {
             InputWidget(
                 category = category.value,
-                amount = amount.value,
+                amount = amountString,
                 setAmount = {
+                    Log.d("InputEntryScreen", "changing to string $it")
+                    amountString = it
                     try {
-                        amount.value = converterDefault.value?.parse(it)?.toDouble() ?: 0.0
+                        amountDouble = it.replace(",", ".").toDouble()
+                        Log.d("InputEntryScreen", "parsing successful, new value $amountDouble")
+                        isValid = true
                     } catch (_: Throwable) {
-
+                        isValid = false
+                        Log.d(
+                            "InputEntryScreen",
+                            "parsing failed, value remains $amountDouble, is valid is $isValid"
+                        )
                     }
                 },
-                changeAmount = { amount.value += it },
+                changeAmount = {
+                    amountDouble += it
+                    amountString = formatCompact(amountDouble)
+                },
                 abort = {
                     navController.popBackStack()
                 },
                 createEntry = {
-                    model.addEntry(amount.value)
-                    navController.popBackStack()
+                    if (isValid) {
+                        model.addEntry(amountDouble)
+                        navController.popBackStack()
+                    }
                 },
-                formatter = {
-                    converterDefault.value?.format(it) ?: it.toString()
-                },
+                isValid = isValid,
                 formatterCompact = {
                     formatCompact(it)
                 }
@@ -78,12 +90,13 @@ fun InputEntryScreen(
 @Composable
 fun InputWidget(
     category: Category? = Category(null, "test", 0.0, 0),
-    amount: Double = 0.0,
+    isValid: Boolean = true,
+    amount: String = "0.0",
     setAmount: (String) -> Unit = {},
     changeAmount: (Double) -> Unit = {},
     createEntry: () -> Unit = {},
     abort: () -> Unit = {},
-    formatter: @Composable (Double) -> String = { "n/a" },
+    //formatter: @Composable (Double) -> String = { "n/a" },
     formatterCompact: (Double) -> String = { it.toString() },
 ) {
     Column(
@@ -98,13 +111,22 @@ fun InputWidget(
         Spacer(modifier = Modifier.height(10.dp))
         Row {
             TextField(
-                value = formatter(amount),
+                value = amount,
                 onValueChange = setAmount,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(5.dp),
                 label = { Text(stringResource(R.string.label_amount)) },
                 singleLine = true,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Decimal
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    if (isValid) {
+                        setAmount(amount)
+                        createEntry()
+                    }
+                })
             )
         }
         Spacer(modifier = Modifier.height(10.dp))
@@ -117,17 +139,47 @@ fun InputWidget(
                     horizontalArrangement = Arrangement.Center,
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    AmountButton(amount = -10.0, changeAmount = changeAmount, formatterCompact)
-                    AmountButton(amount = -5.0, changeAmount = changeAmount, formatterCompact)
-                    AmountButton(amount = -1.0, changeAmount = changeAmount, formatterCompact)
+                    AmountButton(
+                        amount = -10.0,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
+                    AmountButton(
+                        amount = -5.0,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
+                    AmountButton(
+                        amount = -1.0,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
                 }
                 Row(
                     horizontalArrangement = Arrangement.Center,
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    AmountButton(amount = -0.5, changeAmount = changeAmount, formatterCompact)
-                    AmountButton(amount = -0.1, changeAmount = changeAmount, formatterCompact)
-                    AmountButton(amount = -0.01, changeAmount = changeAmount, formatterCompact)
+                    AmountButton(
+                        amount = -0.5,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
+                    AmountButton(
+                        amount = -0.1,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
+                    AmountButton(
+                        amount = -0.01,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
                 }
             }
             Column(modifier = Modifier.weight(1f, true)) {
@@ -135,17 +187,47 @@ fun InputWidget(
                     horizontalArrangement = Arrangement.Center,
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    AmountButton(amount = 10.0, changeAmount = changeAmount, formatterCompact)
-                    AmountButton(amount = 5.0, changeAmount = changeAmount, formatterCompact)
-                    AmountButton(amount = 1.0, changeAmount = changeAmount, formatterCompact)
+                    AmountButton(
+                        amount = 10.0,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
+                    AmountButton(
+                        amount = 5.0,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
+                    AmountButton(
+                        amount = 1.0,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
                 }
                 Row(
                     horizontalArrangement = Arrangement.Center,
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    AmountButton(amount = 0.5, changeAmount = changeAmount, formatterCompact)
-                    AmountButton(amount = 0.1, changeAmount = changeAmount, formatterCompact)
-                    AmountButton(amount = 0.01, changeAmount = changeAmount, formatterCompact)
+                    AmountButton(
+                        amount = 0.5,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
+                    AmountButton(
+                        amount = 0.1,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
+                    AmountButton(
+                        amount = 0.01,
+                        changeAmount = changeAmount,
+                        formatter = formatterCompact,
+                        enabled = isValid
+                    )
                 }
             }
         }
@@ -155,7 +237,8 @@ fun InputWidget(
                 Button(
                     onClick = createEntry, modifier = Modifier
                         .fillMaxWidth()
-                        .padding(5.dp)
+                        .padding(5.dp),
+                    enabled = isValid
                 ) {
                     Text(stringResource(R.string.operation_submit))
                 }
@@ -172,7 +255,12 @@ fun InputWidget(
 }
 
 @Composable
-fun AmountButton(amount: Double, changeAmount: (Double) -> Unit, formatter: (Double) -> String) {
+fun AmountButton(
+    enabled: Boolean,
+    amount: Double,
+    changeAmount: (Double) -> Unit,
+    formatter: (Double) -> String
+) {
     Surface(
         shape = MaterialTheme.shapes.small,
         border = ButtonDefaults.outlinedBorder,
@@ -180,6 +268,13 @@ fun AmountButton(amount: Double, changeAmount: (Double) -> Unit, formatter: (Dou
             .width(64.dp)
             .height(50.dp)
             .padding(2.dp)
+            .let {
+                if (enabled) {
+                    it
+                } else {
+                    it.background(Color.Gray)
+                }
+            }
 
     ) {
         Row(
@@ -191,7 +286,11 @@ fun AmountButton(amount: Double, changeAmount: (Double) -> Unit, formatter: (Dou
                 formatter(amount),
                 fontSize = 12.sp,
                 modifier = Modifier
-                    .clickable(onClick = { changeAmount(amount) }
+                    .clickable(onClick = {
+                        if (enabled) {
+                            changeAmount(amount)
+                        }
+                    }
                     )
                     .padding(5.dp)
                     .background(MaterialTheme.colors.primary),
