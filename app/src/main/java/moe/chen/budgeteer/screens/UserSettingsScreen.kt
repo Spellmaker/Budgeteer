ca6478734e2c1da18c08080e3079f0010cf5749d
Spellmaker <mbts.org@googlemail.com>
2023-06-05 17:32:50 +0200

    added csv export

--- app/src/main/java/moe/chen/budgeteer/screens/UserSettingsScreen.kt
+++ app/src/main/java/moe/chen/budgeteer/screens/UserSettingsScreen.kt
@@ -1,14 +1,37 @@
 package moe.chen.budgeteer.screens
 
+import android.net.Uri
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Save
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,18 +47,34 @@ import moe.chen.budgeteer.viewmodel.UserSettingViewModel
 import moe.chen.budgeteer.widgets.FilteredSelectionWidget
 import moe.chen.budgeteer.widgets.MainViewWidget
 import moe.chen.budgeteer.widgets.OrderSelectionWidget
-import java.util.*
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Currency
+
+private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm")
 
 @Composable
 fun UserSettingsScreen(
     navController: NavController,
 ) {
     val model = hiltViewModel<UserSettingViewModel>()
+    val contentResolver = LocalContext.current.contentResolver
+    val context = LocalContext.current
+    val errorExport = stringResource(id = R.string.error_export)
+    val exportSuccess = stringResource(id = R.string.label_export_success)
     val settings = model.settings.collectAsState()
     if (settings.value != null && settings.value != model.invalidSettings) {
         MainViewWidget(navController = navController) {
             UserSettingEditor(
-                settings.value!!,
+                currentSettings = settings.value!!,
+                exportData = { uri -> model.exportData(uri, contentResolver) {
+                    if (!it) {
+                        Toast.makeText(context, errorExport, Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(context, exportSuccess, Toast.LENGTH_LONG).show()
+                    }
+                }
+                },
                 goBack = { navController.popBackStack() }
             ) {
                 model.updateSettings(it)
@@ -49,6 +88,7 @@ fun UserSettingsScreen(
 @Composable
 fun UserSettingEditor(
     currentSettings: UserSetting = UserSetting.getDefault(0),
+    exportData: (Uri) -> Unit = {},
     goBack: () -> Unit = {},
     updateSettings: (UserSetting) -> Unit = {},
 ) {
@@ -87,29 +127,31 @@ fun UserSettingEditor(
                     }
                 }
 
-                    CategoryRow(
-                        category = Category(
-                            cid = 0,
-                            label = stringResource(R.string.label_example_category),
-                            uid = 0,
-                            order = null,
-                        ),
-                        entries = emptyList(),
-                        clicked = { },
-                        longPress = { },
-                        fields = categoryElements
-                            .filter { it.second >= 0 }
-                            .sortedBy { it.second }
-                            .map { it.first },
-                        categoryToEdit = null,
-                        budget = CategoryBudget(0, 0.0, 0, 0, 0)
-                        ) {
-                        UserSettingViewModel.makeConverter(currentState.currencyCode).format(it)
-                    }
-                //}
+                CategoryRow(
+                    category = Category(
+                        cid = 0,
+                        label = stringResource(R.string.label_example_category),
+                        uid = 0,
+                        order = null,
+                    ),
+                    entries = emptyList(),
+                    clicked = { },
+                    longPress = { },
+                    fields = categoryElements
+                        .filter { it.second >= 0 }
+                        .sortedBy { it.second }
+                        .map { it.first },
+                    categoryToEdit = null,
+                    budget = CategoryBudget(0, 0.0, 0, 0, 0)
+                    ) {
+                    UserSettingViewModel.makeConverter(currentState.currencyCode).format(it)
+                }
 
                 Row(modifier = Modifier.padding(5.dp)) {
-                    Button(onClick = { orderVisible = !orderVisible }, modifier = Modifier.fillMaxWidth()) {
+                    Button(
+                        onClick = { orderVisible = !orderVisible },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
                         if (orderVisible) {
                             Text(stringResource(R.string.operation_stop_editing))
                         } else {
@@ -117,34 +159,17 @@ fun UserSettingEditor(
                         }
                     }
                 }
-                AnimatedVisibility(visible = orderVisible) {
-                    OrderSelectionWidget(
-                        elements = categoryElements,
-                        visibilities = categoryElements
-                            .map { it.second >= 0 },
-                        visibilityChanged = { pos, value ->
-                            if (value) {
-                                val newElements = categoryElements.toMutableList()
-                                newElements[pos] = newElements[pos].first to pos
-                                categoryElements = ensureHiddenLast(newElements)
-                            } else {
-                                val newElements = categoryElements.toMutableList()
-                                newElements[pos] = newElements[pos].first to -1
-                                categoryElements = ensureHiddenLast(newElements)
-                            }
-                        },
-                        orderChanged = { newList ->
-                            categoryElements = ensureHiddenLast(newList)
-                        }
-                    ) { element, modifier ->
-                        Column(modifier = modifier.width(200.dp)) {
-                            Text(stringResource(element.first.label))
-                            Text(
-                                stringResource(element.first.description),
-                                style = MaterialTheme.typography.subtitle1
-                            )
-                        }
-                    }
+                OrderSelector(
+                    orderVisible = orderVisible,
+                    categoryElements = categoryElements,
+                    updateElements = { categoryElements = it }
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+
+                Row(modifier = Modifier.padding(5.dp)) {
+                    ExportButton(handleExport = exportData)
                 }
             }
         },
@@ -183,6 +208,68 @@ fun UserSettingEditor(
             }
         }
     )
+}
+
+@Composable
+fun OrderSelector(
+    orderVisible: Boolean,
+    categoryElements: List<Pair<ComputedField, Int>>,
+    updateElements: (List<Pair<ComputedField, Int>>) -> Unit,
+) {
+    AnimatedVisibility(visible = orderVisible) {
+        OrderSelectionWidget(
+            elements = categoryElements,
+            visibilities = categoryElements
+                .map { it.second >= 0 },
+            visibilityChanged = { pos, value ->
+                if (value) {
+                    val newElements = categoryElements.toMutableList()
+                    newElements[pos] = newElements[pos].first to pos
+                    updateElements(ensureHiddenLast(newElements))
+                    ensureHiddenLast(newElements)
+                } else {
+                    val newElements = categoryElements.toMutableList()
+                    newElements[pos] = newElements[pos].first to -1
+                    updateElements(ensureHiddenLast(newElements))
+                }
+            },
+            orderChanged = { newList ->
+                updateElements(ensureHiddenLast(newList))
+            }
+        ) { element, modifier ->
+            Column(modifier = modifier.width(200.dp)) {
+                Text(stringResource(element.first.label))
+                Text(
+                    stringResource(element.first.description),
+                    style = MaterialTheme.typography.subtitle1
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ExportButton(
+    modifier: Modifier = Modifier,
+    handleExport: (Uri) -> Unit,
+) {
+    val createDocumentResult = rememberLauncherForActivityResult(
+        ActivityResultContracts.CreateDocument("text/csv")
+    ) { uri: Uri? ->
+        if (uri != null) {
+            handleExport(uri)
+        }
+    }
+    Button(
+        modifier = modifier.fillMaxWidth(),
+        onClick = {
+            createDocumentResult.launch(
+                "budgeteer_export_${formatter.format(ZonedDateTime.now())}.csv"
+            )
+        },
+    ) {
+        Text(text = stringResource(R.string.label_export))
+    }
 }
 
 
--- app/src/main/java/moe/chen/budgeteer/viewmodel/UserSettingViewModel.kt
+++ app/src/main/java/moe/chen/budgeteer/viewmodel/UserSettingViewModel.kt
@@ -1,5 +1,7 @@
 package moe.chen.budgeteer.viewmodel
 
+import android.content.ContentResolver
+import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,16 +9,27 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
+import moe.chen.budgeteer.room.BudgetEntryDao
+import moe.chen.budgeteer.room.CategoryBudgetDao
+import moe.chen.budgeteer.room.CategoryDao
 import moe.chen.budgeteer.room.UserSetting
 import moe.chen.budgeteer.room.UserSettingDao
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
 import java.text.NumberFormat
-import java.util.*
+import java.time.format.DateTimeFormatter
+import java.util.Currency
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
 class UserSettingViewModel @Inject constructor(
     private val userSettingDao: UserSettingDao,
+    private val categoryDao: CategoryDao,
+    private val entryDao: BudgetEntryDao,
+    private val budgetDAo: CategoryBudgetDao,
 ) : ViewModel() {
 
     private val userId: Int = 0
@@ -68,6 +81,52 @@ class UserSettingViewModel @Inject constructor(
         }
     }
 
+    fun exportData(uri: Uri, contentResolver: ContentResolver, callback: (Boolean) -> Unit) {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        viewModelScope.launch {
+            try {
+                Log.d("UserSettingViewModel", "Starting file write")
+                contentResolver.openOutputStream(uri)!!.use { os ->
+                    OutputStreamWriter(os).use { oswriter ->
+                        BufferedWriter(oswriter).use { writer ->
+                            Log.d("UserSettingViewModel", "emitting title line")
+                            writer.write("date;amount;title;category;budget;\r\n")
+
+                            val categoryList = categoryDao.listCategories(0).firstOrNull()
+                            categoryList?.forEach { category ->
+                                val entries = entryDao.listEntries(category.cid!!).firstOrNull()
+                                Log.d("UserSettingViewModel", "entries $entries")
+                                entries?.forEach { entry ->
+                                    Log.d(
+                                        "UserSettingViewModel",
+                                        "handling entry $entry"
+                                    )
+                                    val budget = budgetDAo.getActiveBudget(
+                                        categoryId = category.cid,
+                                        year = entry.date.year,
+                                        month = entry.date.monthValue,
+                                    ).firstOrNull()
+
+                                    Log.d("UserSettingViewModel", "emitting line")
+                                    writer.write(
+                                        "${formatter.format(entry.date)};" +
+                                                "${entry.amount};" +
+                                                "${entry.label};" +
+                                                "${category.label};" +
+                                                "${budget?.budget ?: -1};\r\n"
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+                callback(true)
+            } catch(e: Exception) {
+                Log.e("UserSettingViewModel", "error exporting", e)
+                callback(false)
+            }
+        }
+    }
 
     private fun updateConverters(currentSettings: UserSetting) {
         _converterDefault.value = makeConverter(currentSettings.currency)
